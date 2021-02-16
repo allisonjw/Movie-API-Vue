f36350e5d17a062bceaada9d24261a2966c56a0f
Allison Wagner <allisonjw01@hotmail.com>
2021-02-16 14:00:33 -0700

    Adds components to search and view movie by title

---
+++ src/components/Movies.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <section class="section">
+        <h2>{{movies.Title}}</h2>
+          <h2>Rating: <span>{{movies.Rated}}</span></h2>
+          <h2>Year: <span>{{movies.Year}}</span></h2>
+          <h2>Actors: <span>{{movies.Actors}}</span></h2>
+          <h2 class="plot">{{movies.Plot}}</h2>
+          <img :src="movies.Poster" >
+    </section> 
+  </div> 
+</template>
+<script>
+
+  export default {
+    name: 'Movies',
+    props : ['movies']
+  }
+</script>
+
+<style>
+  .section {
+    align-items: center;
+    background-color: #000000c2;;
+    display: flex;
+    flex-direction: column;
+    margin: 2% 12%;
+    border-radius: 1%;
+  }
+  h2 {
+      color: white;
+  }
+  img {
+      width: 30%;
+      margin: 2%;
+  }
+  .plot {
+      width: 50%;
+  }
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h1 class="search_instructions">Please Enter a Movie Title</h1>
+    <form @submit="searchMovies">
+      <input type="text" v-model="search" name="search" placeholder="Movie Title" id="inpt" />
+      <input type="submit" value="SEARCH" id="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Search',
+    data() {
+    return {
+      search: ''
+    }
+  },
+  methods: {
+    searchMovies(e) {
+      e.preventDefault()
+      const newSearch = this.search
+      this.$emit('new-search', newSearch)
+      this.search = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+ #inpt {
+    border: 1px solid black;
+    padding: 10px;
+    width: 300px;
+    font-size: 1.2em;
+  }
+  #btn {
+    background-color: purple;
+    border: none;
+    color: white;
+    padding: 10px 20px;
+    font-size: 1.2em;
+  }
+  #btn:hover {
+    background-color: indigo;
+  }
+  .search_instructions {
+      color: purple;
+      font-size: 3em;
+  }
+</style>
